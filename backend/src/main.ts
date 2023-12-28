679c6d72e91cbf778553429c7a48d124689c3e11
Jang-DongHo <donghoo18@naver.com>
2023-12-28 14:58:46 +0900

    [Project] 프로젝트 생성

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { RequestMethod } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'stage' ? '.env.stage' : '.env.dev',
+  ),
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // API 경로 설정
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'admin', method: RequestMethod.GET }],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('트니버스 API 명세서')
@@ -11,7 +25,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   await app.listen(3000);
 }
