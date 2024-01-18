74fe21bf59b4587fc8f8eac80e520caa20c6a473
Jang-DongHo <donghoo18@naver.com>
2024-01-18 19:06:05 +0900

    [Fix] vote-count view 수정

--- backend/src/vote/entity/vote-count.view.ts
+++ backend/src/vote/entity/vote-count.view.ts
@@ -3,16 +3,20 @@ import { ViewEntity, ViewColumn } from 'typeorm';
 @ViewEntity({
   expression: `
     SELECT
-      vote_acquisition_history.user_id AS id,
-      (COALESCE(SUM(DISTINCT vote_acquisition_history.vote_count), 0) - COALESCE(SUM(DISTINCT vote.vote_count), 0)) AS voteCount
-    FROM
-      vote_acquisition_history
-    JOIN
-      vote
-    ON
-      vote_acquisition_history.user_id = vote.user_id
-    GROUP BY
-      vote_acquisition_history.user_id
+    user_info.id AS id,
+    COALESCE(SUM(DISTINCT vote_acquisition_history.vote_count), 0) - COALESCE(SUM(DISTINCT vote.vote_count), 0) AS voteCount
+  FROM
+    user_info
+  LEFT JOIN
+    vote_acquisition_history
+  ON
+    user_info.id = vote_acquisition_history.user_id
+  LEFT JOIN
+    vote
+  ON
+    user_info.id = vote.user_id
+  GROUP BY
+    user_info.id;
   `,
 })
 export class UserVoteCountView {
