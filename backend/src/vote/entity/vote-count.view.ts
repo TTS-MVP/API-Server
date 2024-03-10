3cc700e238881ff23d3ae6c5edfc9da462ecdcd2
Jang-DongHo <donghoo18@naver.com>
2024-03-10 22:31:12 +0900

    [Fix] 투표권 합산 뷰 식 수정

--- backend/src/vote/entity/vote-count.view.ts
+++ backend/src/vote/entity/vote-count.view.ts
@@ -2,21 +2,25 @@ import { ViewEntity, ViewColumn } from 'typeorm';
 
 @ViewEntity({
   expression: `
-    SELECT
+  SELECT
     user_info.id AS id,
-    COALESCE(SUM(DISTINCT vote_acquisition_history.vote_count), 0) - COALESCE(SUM(vote.vote_count), 0) AS voteCount
+    COALESCE(SUM(vote_acquisition_history.vote_count), 0) - COALESCE(SUM(vote.vote_count), 0) AS voteCount
   FROM
-    user_info
-  LEFT JOIN
-    vote_acquisition_history
-  ON
-    user_info.id = vote_acquisition_history.user_id
-  LEFT JOIN
-    vote
-  ON
-    user_info.id = vote.user_id
-  GROUP BY
-    user_info.id;
+      user_info
+  LEFT JOIN (
+      SELECT user_id, SUM(vote_count) AS vote_count
+      FROM vote_acquisition_history
+      GROUP BY user_id
+  ) AS vote_acquisition_history ON user_info.id = vote_acquisition_history.user_id
+
+  LEFT JOIN (
+      SELECT user_id, SUM(vote_count) AS vote_count
+      FROM vote
+      GROUP BY user_id
+  ) AS vote ON user_info.id = vote.user_id
+
+  GROUP BY user_info.id;
+
   `,
 })
 export class UserVoteCountView {
